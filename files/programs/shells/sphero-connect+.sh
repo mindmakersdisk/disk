6190cc42eb00fd52d755bb85f118c5a0994f9612
mindmakersdisk <48329947+mindmakersdisk@users.noreply.github.com>
2019-03-22 14:22:25 -0300

    Update sphero-connect+.sh

--- files/programs/shells/sphero-connect+.sh
+++ files/programs/shells/sphero-connect+.sh
@@ -3,21 +3,16 @@ sudo fuser -k 80/tcp
 
 echo "********************************************************************************" 
 echo "*                                                                              *" 
-echo "*                 O seu Sphero será conectado!!!                               *" 
+echo "*               O seu Sphero será conectado!                                   *" 
 echo "*                                                                              *" 
-echo "*               Você NÃO poderá fechar esta janela.                            *" 
-echo "*                                                                              *" 
-echo "*               Se fechar a janela, o Sphero poderá apresentar                 *" 
+echo "*               NÃO feche esta janela. Se fechar, o Sphero poderá apresentar   *" 
 echo "*               comportamento indesejado na próxima conexão.                   *" 
 echo "*                                                                              *" 
-echo "********************************************************************************" 
-echo "********************************************************************************" 
+echo "*               Utilize o Desconecta Sphero                                    *" 
 echo "*                                                                              *" 
-echo "*                    Utilize o Desconecta Sphero                               *" 
-echo "*                                                                              *" 
-echo "*            para encerrar a conexão ao terminar as atividades ou              *"
+echo "*               para encerrar a conexão ao terminar as atividades ou           *"
 echo "*                                                                              *"  
-echo "*            em caso de mensagens de erro durante a utilização.                *" 
+echo "*               em caso de mensagens de erro durante a utilização.             *" 
 echo "*                                                                              *" 
 echo "*                                                                              *" 
 echo "********************************************************************************" 
@@ -25,6 +20,5 @@ echo "**************************************************************************
 sleep 5
 
 sudo nodejs /home/mindmakers/programs/mmsphero-server.js F2:98:D7:D9:16:08  
-
 
 sudo fuser -k 80/tcp
