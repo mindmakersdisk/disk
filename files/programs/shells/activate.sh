fb45278476c1393f61fa4efb89d7eeca4662304f
mindmakersdisk <48329947+mindmakersdisk@users.noreply.github.com>
2019-03-22 18:13:29 -0300

    Update activate.sh

--- files/programs/shells/activate.sh
+++ files/programs/shells/activate.sh
@@ -3,7 +3,7 @@ sudo fuser -k 80/tcp
 
 echo "********************************************************************************" 
 echo "*                                                                              *" 
-echo "*      Ativa o Computador para Uso em Sala de Aula por uma Escola              *" 
+echo "*      Ativa o Computador para Uso em Salas de Aula                            *" 
 echo "*                                                                              *" 
 echo "********************************************************************************" 
 
