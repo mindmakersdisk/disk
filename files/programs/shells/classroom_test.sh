ee62a839f3a6340362f0b4c7490d207d930b07d5
mindmakersdisk <48329947+mindmakersdisk@users.noreply.github.com>
2019-04-04 17:21:48 -0300

    Update classroom_test.sh

--- files/programs/shells/classroom_test.sh
+++ files/programs/shells/classroom_test.sh
@@ -6,34 +6,42 @@ sd="$(sudo grep -w "SD:" /home/mindmakers/school.info | sed 's/.*: //')"
 echo pi=$pi \&sd=$sd
 
 run(){
-        echo "Imagem não registrada, registre a imagem para realizar este teste."
+        echo "Imagem não registrada, registre-a para realizar este teste."
         return 0
 }
+if [ -e /home/mindmakers/school.info ]
+  then
+  
+  if [ "$pi" = '' ]
+    then
+  
+    run;
+  
+  elif [ "$pi"  = 'Não registrado' ]
+    then
+  
+    run;
+  
+  else
 
-if [ "$pi" = '' ]
-  then
+    chromium-browser https://mindmakers.cc/instrutor/#/testar-sala-list?pi=$pi\&sd=$sd
   
-  run;
-  
-elif [ "$pi"  = 'Não registrado' ]
-  then
-  
-  run;
+    if [ "$(sudo cat /sys/class/net/eth0/operstate)" = 'up' ]
+      then
+    
+        lxterminal -e vnstat -l
+    elif [ "$(sudo cat /sys/class/net/wlan0/operstate)" = 'up' ]
+      then
+    
+        lxterminal -e vnstat -l -i wlan0
+    fi
+
+  fi
   
 else
-
-  chromium-browser https://mindmakers.cc/instrutor/#/testar-sala-list?pi=$pi\&sd=$sd
+  echo "Imagem não contem arquivo school.info"
+  run;
   
-  if [ "$(sudo cat /sys/class/net/eth0/operstate)" = 'up' ]
-    then
-    
-      lxterminal -e vnstat -l
-  elif [ "$(sudo cat /sys/class/net/wlan0/operstate)" = 'up' ]
-    then
-    
-      lxterminal -e vnstat -l -i wlan0
-  fi
-
 fi
 
 $SHELL
