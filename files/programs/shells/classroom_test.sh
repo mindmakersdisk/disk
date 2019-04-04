d789b6ac00490174dbd886ed46158b8a00560488
mindmakersdisk <48329947+mindmakersdisk@users.noreply.github.com>
2019-04-04 17:00:35 -0300

    Update classroom_test.sh

--- files/programs/shells/classroom_test.sh
+++ files/programs/shells/classroom_test.sh
@@ -3,31 +3,36 @@
 pi="$(sudo grep -w "Pi:" /home/mindmakers/school.info | sed 's/.*: //')" 
 sd="$(sudo grep -w "SD:" /home/mindmakers/school.info | sed 's/.*: //')"  
 
-echo pi=$pi\&sd=$sd
+echo pi=$pi \&sd=$sd
 
 run(){
-        echo "this is run"
+        echo "Imagem não registrada, registre a imagem para realizar este teste."
         return 0
 }
 
 if [ "$pi" = '' ]
   then
-  echo "Imagem não registrada, registre a imagem para realizar este teste."
+  
   run;
+  
 elif [ "$pi"  = 'Não registrado' ]
   then
-  echo "Imagem não registrada, registre a imagem para realizar este teste."
+  
   run;
+  
 else
+  chromium-browser https://mindmakers.cc/instrutor/#/testar-sala-list?pi=$pi\&sd=$sd
+  
   if [ "$(sudo cat /sys/class/net/eth0/operstate)" = 'up' ]
     then
+    
       lxterminal -e vnstat -l
   elif [ "$(sudo cat /sys/class/net/wlan0/operstate)" = 'up' ]
     then
+    
       lxterminal -e vnstat -l -i wlan0
   fi
 
-  chromium-browser https://mindmakers.cc/instrutor/#/testar-sala-list?pi=$pi\&sd=$sd
 fi
 
 $SHELL
