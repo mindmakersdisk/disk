b0bca74dc2b2a27d48fd131e3fb683b1df92ed60
gabrielcbe <gabrielcbe@hotmail.com>
2019-04-10 17:33:44 -0300

    update código
    retirar pasta desnecessária,
    alterar imagem sprk_desconecta,
    alterando +body.err para JSON.stringify(body.err) para mostrar corretamente os erros.
    alterando mmversion update para acomodar atualizações com sala e estação

--- files/programs/mmversionupdate.js
+++ files/programs/mmversionupdate.js
@@ -1,14 +1,14 @@
 /*
   Atualiza versão da estação após atualização de versão de imagem
-   
+
   1. Obtém escola, pi, SD e versão, após atualização Ansible, e gravar na plataforma
-  2. Executado automaticamente em /home/mindmakers/programas -> "sudo node mmversionupdate.js". 
+  2. Executado automaticamente em /home/mindmakers/programas -> "sudo node mmversionupdate.js".
 
   Paulo Alvim 04/2019
   Copyright(c) Mind Makers Editora Educacional Ltda. Todos os direitos reservados
 */
 
-const request = require('request')  
+const request = require('request')
 //const si = require('systeminformation')
 var fs = require('fs');
 
@@ -23,7 +23,7 @@ var sd_registrado='';
 // Identificados
 var versaoImagemDisco='';
 
-fs.readFile('/home/mindmakers/school.info', function(err,data) 
+fs.readFile('/home/mindmakers/school.info', function(err,data)
         {
           if (err) {
               console.log(err);
@@ -35,13 +35,23 @@ fs.readFile('/home/mindmakers/school.info', function(err,data)
              // console.log(escolaid);
              piIni = escolainfo.indexOf('Pi:')+3;
              pi_registrado= escolainfo.substring(piIni,escolainfo.indexOf('||',piIni)).trim();
-             // console.log(pi);
+             // console.log(pi_registrado);
              sdIni = escolainfo.indexOf('SD:')+3;
              sd_registrado= escolainfo.substring(sdIni,escolainfo.indexOf('||',sdIni)).trim();
-             // console.log(sd);
+             //console.log(sd_registrado);
+             sprkIni = escolainfo.indexOf('Sphero:')+7
+             sprk_registrado= escolainfo.substring(sprkIni,escolainfo.indexOf('||',sprkIni)).trim();
+             //console.log(sprk_registrado);
+             salaIni = escolainfo.indexOf('Sala:')+5
+             sala_registrado= escolainfo.substring(salaIni,escolainfo.indexOf('||',salaIni)).trim();
+             //console.log(sala_registrado);
+             estacaoIni = escolainfo.indexOf('Estação:')+8
+             estacao_registrado= escolainfo.substring(estacaoIni,escolainfo.indexOf('||',estacaoIni)).trim();
+             //console.log(estacao_registrado);
             // Verifica se está configurado e se está alocado (pré-requisitos para ativação)
+
             obtemVersaoImagemDisco();
-            
+
             if (escolaid==null || escolaid=='' || pi_registrado=='' || escolaid.toString().toLowerCase().indexOf('não')>-1 ) {
                console.log('');
                console.error('\x1b[31m','Como estação ainda não está ativada, não atualiza versão.');
@@ -49,9 +59,9 @@ fs.readFile('/home/mindmakers/school.info', function(err,data)
                process.exit(1);
       
             }
-            
+
             atualizaVersaoEstacao();
-            
+
           }
         });
 
@@ -64,14 +74,16 @@ fs.readFile('/home/mindmakers/school.info', function(err,data)
 
 
 function atualizaVersaoEstacao() {
-    
+
      request({url: 'https://mindmakers.cc/api/Escolas/atualizaVersaoEstacao/publico',
             method: 'POST',
             json: {
               'alocadoescola':escolaid,
               'computadorserial':pi_registrado,
               'discoserial':sd_registrado,
-              'versaoimagemdisco':versaoImagemDisco}
+              'versaoimagemdisco':versaoImagemDisco
+              'sala':sala_registrado,
+              'codigo':estacao_registrado}
             },
             function(error, response, body){
                 if (!body.success || error) {
@@ -79,50 +91,50 @@ function atualizaVersaoEstacao() {
                       console.log('Erro ao atualizar versão da estação na plataforma: '+JSON.stringify(body.err));
                     else
                       console.log('Erro ao atualizar versão da estação na plataforma: '+error);
-                } else {               
+                } else {
                     console.log('Versão da estação atualizada na plataforma com sucesso! ');
                 }
             }
         );
-    
+
 }
 
 
 
 function statPath(path) {
-  
+
   try {
-    
+
     return fs.statSync(path);
-    
+
   } catch (ex) {
       return false;
-  } 
-  
+  }
+
 }
 
 
 function obtemVersaoImagemDisco() {
-  
+
   var ePortugues = statPath('/home/pi/Área de Trabalho');
-  
+
   if (ePortugues) {
-    
+
     atalho_mm_conteudo= fs.readFileSync('/home/pi/Área de Trabalho/releasenotes.desktop')+'';
-    
+
   } else {
-    
+
     // versão em ingles
-    atalho_mm_conteudo= fs.readFileSync('/home/pi/Desktop/releasenotes.desktop')+'';    
-  
+    atalho_mm_conteudo= fs.readFileSync('/home/pi/Desktop/releasenotes.desktop')+'';
+
   }
-   
+
    // obtém versão
    var inicial = atalho_mm_conteudo.indexOf('Name[pt_BR]=')+12;
-    
+
    var final = atalho_mm_conteudo.indexOf('Type')-1;
-   
-   
+
+
    if (inicial == -1 || final == -1) {
        console.err('Não foi possível identificar a versão da imagem do disco investigando o atalho padrão de notas de liberação');
        console.err('Para atualizar uma estação ela precisa estar usando uma imagem de disco corretamente configurada.');
@@ -130,38 +142,38 @@ function obtemVersaoImagemDisco() {
        // Encerra com falha
        process.exit(1);
    }
-   
+
    versaoImagemDisco = atalho_mm_conteudo.substring(inicial,final);
-   
+
    console.log('Identificada a versão da imagem de disco como '+versaoImagemDisco);
 
-  
+
 }
 
 function existeAtalhoMindMakersPortugues() {
-  
+
   try {
     var ret = statPath('/home/pi/Área de Trabalho/mindmakers.desktop');
     return ret;
   } catch (e) {
     return false;
   }
-  
+
 }
 
 function existeAtalhoMindMakersIngles() {
-  
+
   try {
     var ret =  statPath('/home/pi/Desktop/mindmakers.desktop');
     return ret;
   } catch (e) {
     return false;
   }
-  
+
 }
 
 function existeAtalhoMindMakers() {
- 
+
     return existeAtalhoMindMakersPortugues() || existeAtalhoMindMakersIngles();
-    
+
 }
