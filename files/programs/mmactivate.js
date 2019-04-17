906c139899f4f498f203d23b6e61450aa8902da3
Paulo Alvim <alvim@mindmakers.cc>
2019-04-17 10:56:50 -0300

    maior tolerancia para sphero

--- files/programs/mmactivate.js
+++ files/programs/mmactivate.js
@@ -120,7 +120,7 @@ noble.on('discover', function(peripheral) {
   
   //console.log('Encontrou dispositivos bluetooth low energy (BLE). Scan no. '+numeroScans);
   
-  if (peripheral.rssi>-50  && (
+  if (peripheral.rssi>-65  && (
             (''+peripheral.advertisement.localName).indexOf('SK') == 0 || 
             (''+peripheral.advertisement.localName).indexOf('Sphero')==0)) {
     
