e6e7d6a1fbb95d26d6bfee6c933befb642fa8e16
gabrielcbe <gabrielcbe@hotmail.com>
2019-05-03 08:51:11 -0300

    Update classroom_test.sh

--- files/programs/shells/classroom_test.sh
+++ files/programs/shells/classroom_test.sh
@@ -9,44 +9,48 @@ echo pi=$pi \& sd=$sd
 
 #condição de saída, termina a execução mas o terminal continua aberto
 run(){
-        echo "Imagem não registrada, registre-a para realizar este teste."
-        return 0
+  echo "Finalizando programa"
+  return 0
 }
 
 #testa se o arquivo school.info existe
 if [ -e /home/mindmakers/school.info ]
-  then
+then
 
   #termina o programa se pi for vazio (geralmente alocado para escola mas não registrado)
   if [ "$pi" = '' ]
-    then
-
+  then
+    echo "Imagem não registrada, registre-a para realizar este teste."
     run;
 
-  #termina o programa se pi for não registrado
+    #termina o programa se pi for não registrado
   elif [ "$pi"  = 'Não registrado' ]
-    then
-
+  then
+    echo "Imagem não registrada, registre-a para realizar este teste."
     run;
 
   else
 
     #se Pi registrado, e estiver ligado por cabo na rede (eth0), roda vnstat como eth0
     if [ "$(sudo cat /sys/class/net/eth0/operstate)" = 'up' ]
-      then
+    then
 
-        lxterminal -e vnstat -l
+      lxterminal -e vnstat -l
 
-    #se Pi registrado, mas está ligado a rede por WiFi (wlan0), roda vnstat como wlan0
+      #se Pi registrado, mas está ligado a rede por WiFi (wlan0), roda vnstat como wlan0
     elif [ "$(sudo cat /sys/class/net/wlan0/operstate)" = 'up' ]
-      then
+    then
 
-        lxterminal -e vnstat -l -i wlan0
+      lxterminal -e vnstat -l -i wlan0
+      
+    else
+      echo "Nenhuma conexão de rede encontrada"
+      run;
+
     fi
 
     #abre chromium browser em anónimo para o teste de sala e já passa o serial do Pi e do cartão SD como valores.
     chromium-browser https://mindmakers.cc/instrutor/#/testar-sala-list?pi=$pi\&sd=$sd --incognito
-
 
   fi
 
