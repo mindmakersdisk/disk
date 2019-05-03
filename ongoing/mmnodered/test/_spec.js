c3df64e93d159ae0856f8f5cbb23f751201df41d
Paulo Alvim <alvim@mindmakers.cc>
2019-05-03 08:59:13 -0300

    node-red

---
+++ ongoing/mmnodered/test/_spec.js
@@ -0,0 +1,33 @@
+var should = require("should");
+var helper = require("node-red-test-helper");
+var lowerNode = require("../mm-littlebits.js");
+
+describe('mm-littlebits Node', function () {
+
+  afterEach(function () {
+    helper.unload();
+  });
+
+  it('deve ser carregado', function (done) {
+    var flow = [{ id: "n1", type: "mm-littlebits", name: "testa nome" }];
+    helper.load(lowerNode, flow, function () {
+      var n1 = helper.getNode("n1");
+      n1.should.have.property('name', 'testa nome');
+      done();
+    });
+  });
+
+  it('deve receber uma mensagem', function (done) {
+    var flow = [{ id: "n1", type: "mm-littlebits", name: "testa nome",wires:[["n2"]] },
+    { id: "n2", type: "helper" }];
+    helper.load(lowerNode, flow, function () {
+      var n2 = helper.getNode("n2");
+      var n1 = helper.getNode("n1");
+      n2.on("input", function (msg) {
+        msg.should.have.property('payload', 'uppercase');
+        done();
+      });
+      n1.receive({ payload: "UpperCase" });
+    });
+  });
+});
