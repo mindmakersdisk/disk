7848b6008f7b1d5e542e9a93bfc0160ffa67d89d
gabrielcbe <gabrielcbe@hotmail.com>
2019-05-07 18:19:43 -0300

    mqtt service

--- files/programs/shells/mqttbot.sh
+++ files/programs/shells/mqttbot.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-sleep 10
-sudo python /home/mindmakers/programs/mmiotcli.py
+sleep 30
+NAME=mqttbot
+if pgrep -f mmiotcli.py > /dev/null
+then
+  echo "$NAME is now running"
+else
+  echo ''
+  echo "Error! Could not start $NAME!"
+  echo "trying again"
+  sudo python /home/mindmakers/programs/mmiotcli.py &>/var/log/mqttbot.log & echo \$!
+fi
