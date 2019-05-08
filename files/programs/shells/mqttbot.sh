5a80c6f1979b39da6ff0404c0698c282fc4b4d8b
gabrielcbe <gabrielcbe@hotmail.com>
2019-05-08 11:59:27 -0300

    Update mqttbot.sh

--- files/programs/shells/mqttbot.sh
+++ files/programs/shells/mqttbot.sh
@@ -4,10 +4,10 @@ sleep 30
 NAME=mqttbot
 if pgrep -f mmiotcli.py > /dev/null
 then
-  echo "$NAME is now running"
+  echo "$NAME já está rodando (via shell)"
 else
   echo ''
-  echo "Error! Could not start $NAME!"
-  echo "trying again"
+  echo "Erro! Não consegui iniciar $NAME!"
+  echo "tentando novamente(via shell)"
   sudo service mqttbot restart
 fi
