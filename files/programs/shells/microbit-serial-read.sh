2aa6375e8b8841864ed3175111c95d829025672a
mindmakersdisk <48329947+mindmakersdisk@users.noreply.github.com>
2019-05-08 14:38:07 -0300

    Add files via upload

--- files/programs/shells/microbit-serial-read.sh
+++ files/programs/shells/microbit-serial-read.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+echo "O programa irá coletar dados do micro:bit por 1 minuto."
+echo ""
+echo "E, após este tempo, irá gravar o arquivo "
+echo ""
+echo "screenlog.0 no ~/Desktop com os valores coletados"
+sleep 5
+sudo bash /home/mindmakers/programs/shells/microbit-serial-kill.sh &
 rm -f ~/Desktop/screenlog.0
 cd ~/Desktop
-screen -L /dev/ttyACM0 115200
-sleep 120
-screen -X kill
+screen -L screenlog.0 /dev/ttyACM0 115200
+
+$SHELL
