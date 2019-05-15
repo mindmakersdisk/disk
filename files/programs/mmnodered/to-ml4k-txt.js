8b35eae78da424fbb60a3ade8e85ff56d505d2d6
mindmakersdisk <48329947+mindmakersdisk@users.noreply.github.com>
2019-05-15 17:12:22 -0300

    Add files via upload

---
+++ files/programs/mmnodered/to-ml4k-txt.js
@@ -0,0 +1,66 @@
+module.exports = function(RED) {
+
+  const URL_BASE = "https://machinelearningforkids.co.uk/api/scratch/";
+
+  console.log('##### mind-makers nodered-to-ml4k-txt instalado');
+
+  function ML4KOutput(config) {
+
+    RED.nodes.createNode(this,config);
+
+    this.key = config.key;
+    this.phrase = config.phrase;
+
+    //console.log('entrou configurar '+this.acao+' estacao '+this.estacao);
+
+    var node = this;
+
+    const request = require('request');
+        
+        node.on('input', function(msg) {
+            
+            if (msg.payload) {
+                
+                if (isNaN(msg.payload+'')) {
+                    node.numero = msg.payload;
+                }
+            
+            }
+            
+            //var URL_BASE = "https://machinelearningforkids.co.uk/api/scratch/";
+    
+            var jsonMsg = {"data" : node.phrase};
+                              
+            //console.log('entrou para enviar msg',jsonMsg);
+            
+            request({url: URL_BASE+node.key+'/classify',
+                    method: 'POST',
+                      json: jsonMsg},    
+                    function(error, response, body){ 
+            //             console.log(body);             
+                        if (error) {
+                           console.log('Erro ao enviar comando - erro: ',error);
+                           var msg = {payload:error};
+                           node.send(msg); 
+                        } else if (body.error) {
+                           console.log('Erro ao enviar comando - corpo do erro: ',body.error);
+                           var msg = {payload:body.error};
+                           node.send(msg);                                                   
+                        } else {
+                              //console.log('body ',body);
+                              var msg = {payload: body[0].class_name, result: body[0].class_name, confidence: body[0].confidence+'%'};
+                              node.send(msg);
+                              console.log('Resposta: ',body[0].class_name);
+                              console.log('Confiança: ',+body[0].confidence,'%');
+                            
+                        }
+                    }
+                );
+        
+        });
+
+  }
+
+RED.nodes.registerType("to-ml4k-txt",ML4KOutput);
+
+}
