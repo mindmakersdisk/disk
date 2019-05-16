c08b4e20b13ee2840260320b0cb8205217f9ee8b
mindmakersdisk <48329947+mindmakersdisk@users.noreply.github.com>
2019-05-16 16:02:25 -0300

    Update camera.sh

--- files/programs/shells/camera.sh
+++ files/programs/shells/camera.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 data=$(date +%d%b%Y-%T)
+caminho=/home/pi/Pictures
 
-Exec= raspistill -t 5000 -o  /home/pi/Desktop/imagem.jpg -w 500 -h 300
+echo $caminho/$data.jpg 2>&1
+
+Exec= raspistill -t 7000 -o  $caminho/$data.jpg -w 500 -h 300
