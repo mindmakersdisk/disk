1266756d2862dd034586e0d4650391b770ac2db7
gabrielcbe <gabrielcbe@hotmail.com>
2019-05-17 09:19:59 -0300

    melhoras cópia

--- files/programs/shells/camera.sh
+++ files/programs/shells/camera.sh
@@ -3,6 +3,6 @@
 data=$(date +%d%b%Y-%T)
 caminho=/home/pi/Pictures
 
-echo $caminho/$data.jpg 2>&1
+raspistill -t 7000 -o  $caminho/$data.jpg -w 500 -h 300
 
-Exec= raspistill -t 7000 -o  $caminho/$data.jpg -w 500 -h 300
+echo $caminho/$data.jpg 2>&1
