286aaa83b3abc1bae9eae72b992c867d1a59a77d
mindmakersdisk <48329947+mindmakersdisk@users.noreply.github.com>
2019-05-17 16:11:11 -0300

    Add files via upload

--- files/programs/shells/camera.sh
+++ files/programs/shells/camera.sh
@@ -1,8 +1,52 @@
 #!/bin/bash
 
+pergunta(){
+echo -n "Qual o ip do smartphone? (ex.: 192.168.100.44) "
+local SURE
+read SURE
+resposta=$SURE
+}
+run(){
+  echo "Finalizando programa"
+  return 1
+}
+
 data=$(date +%d%b%Y-%T)
 caminho=/home/pi/Pictures
 
-raspistill -t 7000 -o  $caminho/$data.jpg -w 500 -h 300
+if [ "$(sudo vcgencmd get_camera | sed 's/supported=1 detected=//')" = '1' ]
+then
 
-echo $caminho/$data.jpg 2>&1
+	raspistill -t 7000 -o  $caminho/$data.jpg -w 500 -h 300
+	echo $caminho/$data.jpg 2>&1
+
+else
+ 
+	if [ "$(adb shell ip addr show wlan0 | grep "inet\s" | awk '{print $2}' | awk -F'/' '{print $1}' | wc -l )" = 1 ]
+	then
+	    #se existir adb conectado pega o ip dele.
+		resposta=$(adb shell ip addr show wlan0 | grep "inet\s" | awk '{print $2}' | awk -F'/' '{print $1}')
+	else
+		#caso não tenha adb conectado, pede pro usuário.
+		# pega primeiro argumento (se existir e passa como resposta)
+		ip=$1
+		
+		if [ -n "$ip" ]
+		then
+			resposta=$ip
+		else
+			pergunta;
+		fi
+	fi
+	
+	
+	if [ -n "$resposta" ]
+	then
+		sudo wget -O $caminho/$data.jpg "$resposta:8080/photoaf.jpg"
+		echo $caminho/$data.jpg 2>&1
+	else
+		echo "Nenhum caminho informado, tente novamente informando um Ip válido!"
+		run;
+	fi
+
+fi
