c5d6bc9f43c241ad3b7cc57f50e74e072e479a28
mindmakersdisk <48329947+mindmakersdisk@users.noreply.github.com>
2019-05-20 12:18:56 -0300

    Classificar e treinar ml4k no node-red

--- files/programs/mmnodered/to-ml4k-img.js
+++ files/programs/mmnodered/to-ml4k-img.js
@@ -10,27 +10,34 @@ module.exports = function(RED) {
 
     this.key = config.key;
     this.locationOfImg = config.locationOfImg;
+    this.key = config.key;
+    this.acao = config.acao;
+    this.label = config.label;
 
-    //console.log('entrou configurar '+this.acao+' estacao '+this.estacao);
     
-    var node = this;
+    let node = this;
 
     const request = require('request');
     var fs = require('fs');
 
     node.on('input', function(msg) {
+      
+      //TODO, validar que msg.payload começa com / e termina com .jpg ou .png
 
-      if (msg.payload) {
+      if (isNaN(msg.payload+'')) {
         msg.payload = msg.payload.replace(/[\n\t\r]/g,"");
         //retirar \n no final da mensagem camera.sh
 
         if(lastMsg !=''){
            //verifica se exite uma ultima msg e substitui pela atual.
            node.locationOfImg = msg.payload;
-           //console.log('lastMsg exite');
+
            //console.log('lastMsg exite',msg.payload);
+           
         }else if (node.locationOfImg=='') {
+          
           node.locationOfImg = msg.payload;
+          
           var lastMsg = msg.payload;
           //armazena a ultima mensagem para comparação.
         }
@@ -41,13 +48,29 @@ module.exports = function(RED) {
       //console.log('bitmap',bitmap);
       // convert binary data to base64 encoded string
       var img = new Buffer(bitmap).toString('base64');
+      
+      //TODO, parar se (node.acao == 'treinar' && node.locationOfImg == '')
+      
+      
+      node.acao = node.acao.replace(/[\n\t\r]/g,"");
+      //precisa retirar para funcionar.
+              
 
-
-      var jsonMsg = {"data" : img};
+      
+      if(node.acao == 'T' ){
+        var jsonMsg = {"data" : img,
+                       "label": node.label };
+        var URL_FINAL = '/train';
+      }else{
+        var jsonMsg = {"data" : img};
+        var URL_FINAL = '/classify';
+      }
+        //console.log('entrou classificar', node.label);
+        //console.log('URL_FINAL ', URL_FINAL)
 
       //console.log('entrou para enviar msg',jsonMsg);
 
-      request({url: URL_BASE+node.key+'/classify',
+      request({url: URL_BASE+node.key+URL_FINAL,
       method: 'POST',
       json: jsonMsg},
       function(error, response, body){
@@ -62,10 +85,21 @@ module.exports = function(RED) {
           node.send(msg);
         } else {
           //console.log('body ',body);
-          var msg = {payload: 'result: '+body[0].class_name+' & confidence: '+body[0].confidence+'%', result: body[0].class_name, confidence: body[0].confidence+'%'};
+          
+          if(node.acao == 'T' ){
+             var msg = {payload: 'stored: '+body.isstored+' & label: '+body.label, 
+                        stored: body.isstored, 
+                        label: body.label};
+             
+           }else{
+             var msg = {payload: 'result: '+body[0].class_name+' & confidence: '+body[0].confidence+'%', 
+                        result: body[0].class_name, 
+                        confidence: body[0].confidence+'%'};
+           }
+          
+          console.log('msg ',msg);
           node.send(msg);
-          console.log('Resposta: ',body[0].class_name);
-          console.log('Confiança: ',+body[0].confidence,'%');
+          
 
         }
       }
