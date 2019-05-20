e360ad987d0c6fde7a80b969ed1f06e6412c87a2
mindmakersdisk <48329947+mindmakersdisk@users.noreply.github.com>
2019-05-20 15:37:16 -0300

    melhorias em geral

--- files/programs/shells/gnu_plot-mt5.sh
+++ files/programs/shells/gnu_plot-mt5.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+sudo cat screenlog.0 | grep ',' | wc -l
+
+if [ "$(sudo cat screenlog.0 | grep -c ',')" -gt 0 ]
+	then
+	    sudo sed -i -e 's/set datafile separator " "/set datafile separator ","/g' /home/mindmakers/programs/shells/script-grafico.dat 
+	else
+		sudo sed -i -e 's/set datafile separator ","/set datafile separator " "/g' /home/mindmakers/programs/shells/script-grafico.dat
+	fi
+
 sudo gnuplot -e "cd '/home/mindmakers/programs/shells/';load 'script-grafico.dat';exit"
