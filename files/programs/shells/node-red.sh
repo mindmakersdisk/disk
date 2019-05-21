2722fb5ef1b8b0f5fe951d50011a73d9b928a0b9
gabrielcbe <gabrielcbe@hotmail.com>
2019-05-21 11:00:29 -0300

    teste nova instalação ansible via pip

--- files/programs/shells/node-red.sh
+++ files/programs/shells/node-red.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo /usr/bin/node-red-stop >> /dev/null 2>&1 &
+sleep 2
 bash /home/mindmakers/programs/shells/node-red-localhost.sh >> /dev/null 2>&1 &
 sudo /usr/bin/node-red-start
 
