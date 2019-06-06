d7cdad68a0babf62f3d99cdfbdf89a28aa171f58
mindmakersdisk <48329947+mindmakersdisk@users.noreply.github.com>
2019-06-06 11:55:51 -0300

    Add files via upload

--- files/programs/shells/gnu_plot-mt5.sh
+++ files/programs/shells/gnu_plot-mt5.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if [ -e /home/pi/Desktop/screenlog.0 ]
+then
 sed -i -r -e 's/([0-9]+)\s+?,?\s+?([0-9]+)\s+?/\1,\2,/' /home/pi/Desktop/screenlog.0
 
 sudo gnuplot -e "cd '/home/mindmakers/programs/shells/';load 'script-grafico.dat';exit"
+
+else
+echo "Arquivo screenlog.0 não encontrado"
+echo "Saindo do programa"
+echo ""
+sleep 5
+
+fi
