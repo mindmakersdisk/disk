4924df1bb982d9b8a7152eb853aeaaf416dbdc27
gabrielcbe <gabrielcbe@hotmail.com>
2019-06-29 09:57:55 -0300

    teste reiniciar mqtt na primeira conexao do dia para garantir servico

--- files/programs/shells/mqttbot.sh
+++ files/programs/shells/mqttbot.sh
@@ -5,6 +5,13 @@ NAME=mqttbot
 if pgrep -f mmiotcli.py > /dev/null
 then
   echo "$NAME já está rodando (via shell)"
+
+  if [ -e /tmp/restart-mqtt.txt ] && [ $(cat /tmp/restart-mqtt.txt | wc -m ) -gt 1 ]
+  then
+    echo 'mqtt vai reiniciar na primeira execução para garantir funcionamento' > /tmp/restart-mqtt.txt
+    sudo service mqttbot restart >/var/log/mqttbot.log
+  fi
+
 else
   echo ''
   echo "Erro! Não consegui iniciar $NAME!"
