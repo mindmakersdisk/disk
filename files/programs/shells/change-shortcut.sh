2df2b23b090a7cb65ee3d26c330ab2a46029734f
Gabriel Becheleni <43211813+gabrielcbe@users.noreply.github.com>
2019-07-02 12:46:27 -0300

    proteção e desproteção mindmakers.desktop

--- files/programs/shells/change-shortcut.sh
+++ files/programs/shells/change-shortcut.sh
@@ -5,11 +5,12 @@ then
 
    sudo chattr -i /home/pi/Área\ de\ Trabalho/classroom_test.desktop
    sudo chattr -i /home/pi/Área\ de\ Trabalho/activate.desktop   
+   sudo chattr -i /home/pi/Área\ de\ Trabalho/mindmakers.desktop
 
 else
 
    sudo chattr -i /home/pi/Desktop/classroom_test.desktop
-   sudo chattr -i /home/pi/Desktop/activate.desktop   
+   sudo chattr -i /home/pi/Desktop/activate.desktop
+   sudo chattr -i /home/pi/Desktop/mindmakers.desktop   
 
 fi
-
