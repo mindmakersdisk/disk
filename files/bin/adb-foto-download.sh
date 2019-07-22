c36ff6801622b5d9d72be8440bf2ba8c05c0232d
Gabriel Becheleni <43211813+gabrielcbe@users.noreply.github.com>
2019-07-22 16:15:19 -0300

    Add files via upload

--- files/bin/adb-foto-download.sh
+++ files/bin/adb-foto-download.sh
@@ -5,7 +5,9 @@ EXTENSION=".jpg"
 i=0
 
 pergunta(){
-  echo -n "quantas fotos quer recuperar? "
+  echo ""
+  echo "Preparando para o download das fotos do celular via adb."
+  echo -n "Quantas fotos desja recuperar? "
   echo ""
   local SURE
   read SURE
@@ -18,12 +20,17 @@ pergunta;
 for file in $(adb shell ls -t $DEVICE_DIR | grep $EXTENSION'$')
 do
     #para cada arquivo listado enquanto i<=$resposta, puxa o arquivo para pasta \tmp e
+    
     #converte para um tamanho menor gravando esse arquivo na pasta padrão "HOST_DIR"
     let i=$i+1
     if [ "$i" -le $resposta ]
     then
         adb pull "$DEVICE_DIR/$file" "/tmp/$file";
-        convert "/tmp/$file" -resize 720x480 "$HOST_DIR/$file"
+        
+        #comando abaixo retira metadados das fotos se descomentado
+        #mogrify -quiet -strip "/tmp/$file"
+        
+        convert -quiet "/tmp/$file" -resize 720x480 "$HOST_DIR/$file"
     fi
 
 done
