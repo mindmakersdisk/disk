070ba42f92e82868239b2f6ee99c7ad0b269eaef
gabrielcbe <gabrielcbe@hotmail.com>
2019-07-22 08:40:48 -0300

    audio1.0 e foto

---
+++ files/programs/shells/adb-foto-download.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+HOST_DIR=/home/pi/Pictures
+DEVICE_DIR=/sdcard/DCIM/Camera
+EXTENSION=".jpg"
+i=0
+
+pergunta(){
+  echo -n "quantas fotos quer recuperar? "
+  echo ""
+  local SURE
+  read SURE
+  resposta=$SURE
+}
+
+pergunta;
+
+#adb vai na pasta "DEVICE_DIR" e lista todos arquivos em ordem de mais novo para mais antigo
+for file in $(adb shell ls -t $DEVICE_DIR | grep $EXTENSION'$')
+do
+    #para cada arquivo listado enquanto i<=$resposta, puxa o arquivo para pasta \tmp e
+    #converte para um tamanho menor gravando esse arquivo na pasta padrão "HOST_DIR"
+    let i=$i+1
+    if [ "$i" -le $resposta ]
+    then
+        adb pull "$DEVICE_DIR/$file" "/tmp/$file";
+        convert "/tmp/$file" -resize 720x480 "$HOST_DIR/$file"
+    fi
+
+done
--- ongoing/adb-foto-download.sh
+++ ongoing/adb-foto-download.sh
@@ -15,17 +15,15 @@ pergunta(){
 pergunta;
 
 #adb vai na pasta "DEVICE_DIR" e lista todos arquivos em ordem de mais novo para mais antigo
-
 for file in $(adb shell ls -t $DEVICE_DIR | grep $EXTENSION'$')
 do
     #para cada arquivo listado enquanto i<=$resposta, puxa o arquivo para pasta \tmp e
     #converte para um tamanho menor gravando esse arquivo na pasta padrão "HOST_DIR"
     let i=$i+1
     if [ "$i" -le $resposta ]
-    then   
+    then
         adb pull "$DEVICE_DIR/$file" "/tmp/$file";
         convert "/tmp/$file" -resize 720x480 "$HOST_DIR/$file"
     fi
-    
-done
 
+done
