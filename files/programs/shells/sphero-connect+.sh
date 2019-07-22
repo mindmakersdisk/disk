abfa59f2f71c860a64d56ddc673f4a9973f79fc0
gabrielcbe <gabrielcbe@hotmail.com>
2019-07-22 17:24:30 -0300

    refatorando textos

--- files/programs/shells/sphero-connect+.sh
+++ files/programs/shells/sphero-connect+.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sudo fuser -k 80/tcp
 
-echo "********************************************************************************" 
-echo "*                                                                              *" 
-echo "*               Serviço de configuração e conexão do Sphero!                   *" 
-echo "*                                                                              *" 
-echo "********************************************************************************" 
+echo "********************************************************************************"
+echo "*                                                                              *"
+echo "*                 Serviço de configuração e conexão do Sphero!                 *" 
+echo "*                                                                              *"
+echo "********************************************************************************"
 
 sudo nodejs /home/mindmakers/programs/mmsphero-server.js
 
