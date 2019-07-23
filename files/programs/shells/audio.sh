bd4743d0479f87c50cd5e6fb91ec03babded9465
gabrielcbe <gabrielcbe@hotmail.com>
2019-07-23 16:10:50 -0300

    evolução bash validando ip celular com ip raspberry

--- files/programs/shells/audio.sh
+++ files/programs/shells/audio.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
+raspip=$(hostname -I | awk -F'.' '{print $1,$2}' OFS='.')
+
 pergunta(){
+  echo ""
   echo -n "Qual o IP do smartphone, mostrado no aplicativo IpWebcam? (Ex.: 192.168.100.40:8080) "
   echo ""
 
   while :; do
      local SURE
      read SURE
+     sureip=$(echo $SURE | awk -F'.' '{print $1,$2}' OFS='.')
+
      if [[ $SURE =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\:8080$ ]];
      then
-       resposta=$SURE && break
+       if [ "$raspip" = "$sureip" ]
+       then
+         resposta=$SURE && break
+       else
+         echo "O Smartphone não se encontra na mesma rede(WIFI) do Raspberry, "
+         echo "certifique-se que eles estão na mesma rede e digite o ip novamente"
+         echo ""
+       fi
+
      else
+       echo ""
        echo "Endereço IP inválido, favor verificar se o endereço digitato tem o formato exemplificado"
        echo "Ex.: 192.168.100.40:8080"
        echo ""
@@ -22,6 +36,7 @@ pergunta(){
 
 run(){
   echo "Finalizando programa"
+  sleep 5
   return 1
 }
 
@@ -30,9 +45,19 @@ caminho=/home/pi/Music
 
   if [ "$(adb shell ip addr show wlan0 | grep "inet\s" | awk '{print $2}' | awk -F'/' '{print $1}' | wc -l )" = 1 ]
   then
-    #se existir adb conectado pega o ip dele.
-    resposta=$(adb shell ip addr show wlan0 | grep "inet\s" | awk '{print $2}' | awk -F'/' '{print $1}')
-    resposta="$resposta:8080"
+    adbip=$(adb shell ip addr show wlan0 | grep "inet\s" | awk '{print $2}' | awk -F'.' '{print $1,$2}' OFS='.')
+    if [ "$raspip" = "$adbip" ]
+    then
+      #se existir adb conectado pega o ip dele.
+      resposta=$(adb shell ip addr show wlan0 | grep "inet\s" | awk '{print $2}' | awk -F'/' '{print $1}')
+      resposta="$resposta:8080"
+    else
+      echo "O Smartphone não se encontra na mesma rede(WIFI) do Raspberry, "
+      echo "certifique-se que eles estão na mesma rede e tente novamente"
+      echo ""
+      run;
+    fi
+
   else
     #caso não tenha adb conectado, pede pro usuário.
     # pega primeiro argumento (se existir e passa como resposta)
@@ -59,7 +84,14 @@ caminho=/home/pi/Music
 
     #sleep 3
     #cvlc://quit
-    echo $caminho/$data.wav 2>&1
+    if [ -s  $caminho/$data.wav ]
+    then
+      echo $caminho/$data.wav 2>&1
+    else
+      echo "Erro ao fazer download do arquivo"
+      echo "Favor tentar novamente, caso o erro persista Reinicie o Raspberry e tente novamente."
+    fi
+
   else
     echo "Nenhum caminho informado, tente novamente informando um IP válido!"
     run;
