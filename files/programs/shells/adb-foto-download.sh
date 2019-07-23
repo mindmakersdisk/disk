4139837c7efa3319fc98cb0cb05c78afb6317c60
gabrielcbe <gabrielcbe@hotmail.com>
2019-07-23 11:25:51 -0300

    refatorando códigos

--- files/programs/shells/adb-foto-download.sh
+++ files/programs/shells/adb-foto-download.sh
@@ -6,10 +6,10 @@ i=0
 
 pergunta(){
   echo "********************************************************************************"
-  echo "*           Preparando para o download das fotos do celular via adb.           *"
+  echo "*       Preparando para o download das fotos do celular Android via adb.       *"
   echo "*   Caso o diretório onde as fotos se encontram no celular seja diferente de   *"
   echo "*                             '/sdcard/DCIM/Camera'                            *"
-  echo "*                favor executar a shell explicitando o caminho                 *"
+  echo "*     favor executar a shell explicitando o caminho, como no exemplo abaixo    *"
   echo "*                  'adb-foto-download.sh caminho/para/pasta'                   *"
   echo "********************************************************************************"
   echo ""
@@ -18,13 +18,13 @@ pergunta(){
   while :; do
      local SURE
      read SURE
-     if [[ $SURE =~ ^[0-9]{1,2}$ ]];
+     if [[ $SURE =~ ^[0-9]{1,2}$ ]] && [ $SURE -gt 0 ];
      then
-     resposta=$SURE && break
+       resposta=$SURE && break
      else
-     echo "Quantidade digitada não é um número, digite valores entre 1 e 99"
-     echo ""
-     echo -n ""
+       echo "Quantidade digitada não é um número, digite valores entre 1 e 99"
+       echo ""
+       echo -n ""
      fi
   done
 }
@@ -35,31 +35,57 @@ dir=$1
 
 if [ -n "$dir" ]
 then
+
   DEVICE_DIR=$dir
+
 else
+
    if [ -e /var/log/adb-caminho.txt ] && [ $(cat /var/log/adb-caminho.txt | wc -m ) -gt 1 ]
    then
-     export DEVICE_DIR=`cat /tmp/ipwebcam.txt`
+     export DEVICE_DIR=`cat /var/log/adb-caminho.txt`
    fi
+
 fi
 
 pergunta;
-echo $DEVICE_DIR > /var/log/adb-caminho.txt
 
-for file in $(adb shell ls -t $DEVICE_DIR | grep $EXTENSION'$')
-do
-    #para cada arquivo listado enquanto i<=$resposta, puxa o arquivo para pasta \tmp e
+echo ""
+echo "A pasta selecionada é: "
+echo $DEVICE_DIR | sudo tee /var/log/adb-caminho.txt
+echo ""
 
-    #converte para um tamanho menor gravando esse arquivo na pasta padrão "HOST_DIR"
-    let i=$i+1
-    if [ "$i" -le $resposta ]
-    then
-        adb pull "$DEVICE_DIR/$file" "/tmp/$file";
+qt=$(adb shell ls -t $DEVICE_DIR | grep $EXTENSION'$'| wc -l)
 
-        #comando abaixo retira metadados das fotos se descomentado
-        #mogrify -quiet -strip "/tmp/$file"
+if [ $qt -lt $resposta ] && [ $qt -gt 0 ]
+then
+  resposta=$qt
+  echo "A pasta informada possúi '$qt' fotos."
+  echo ""
+fi
 
-        convert -quiet "/tmp/$file" -resize 720x480 "$HOST_DIR/$file"
-    fi
+if [ $qt -gt 0 ]
+then
 
-done
+  for file in $(adb shell ls -t $DEVICE_DIR | grep $EXTENSION'$')
+  do
+      #para cada arquivo listado enquanto i<=$resposta, puxa o arquivo para pasta \tmp e
+      #converte para um tamanho menor gravando esse arquivo na pasta padrão "HOST_DIR"
+      let i=$i+1
+      if [ "$i" -le $resposta ]
+      then
+          sudo adb pull "$DEVICE_DIR/$file" "/tmp/$file";
+
+          #comando abaixo retira metadados das fotos se descomentado
+          #mogrify -quiet -strip "/tmp/$file"
+
+          sudo convert -quiet "/tmp/$file" -resize 720x480 "$HOST_DIR/$file"
+      fi
+
+  done
+
+else
+
+  echo "A pasta selecionada não possúi nenhum arquivo .jpg"
+  echo "Encerrando o programa."
+
+fi
--- files/programs/shells/microbit-serial-read.sh
+++ files/programs/shells/microbit-serial-read.sh
@@ -13,31 +13,34 @@ pergunta(){
   resposta=$SURE
 }
 
-echo "O programa irá coletar dados do micro:bit."
-echo ""
-echo "Que devem estar em formato:"
-echo " tempo1,luminosidade1"
-echo " tempo2,luminosidade2"
-echo " ..."
-echo ""
-echo "O arquivo screenlog.0 será gravado no ~/Desktop com os valores coletados."
-echo ""
+echo "********************************************************************************"
+echo "*                  O programa irá coletar dados do micro:bit.                  *"
+echo "*                                                                              *"
+echo "* Que devem estar em formato:                                                  *"
+echo "* tempo1,luminosidade1                                                         *"
+echo "* tempo2,luminosidade2                                                         *"
+echo "* ...                                                                          *"
+echo "*                                                                              *"
+echo "*   O arquivo screenlog.0 será gravado no ~/Desktop com os valores coletados.  *"
+echo "********************************************************************************"
+
 pergunta;
 
 if [ -e /dev/ttyACM0 ]
 then
 
-sudo bash /home/mindmakers/programs/shells/microbit-serial-kill.sh $resposta &
-rm -f ~/Desktop/screenlog.0
-cd ~/Desktop
-screen -L -Logfile screenlog.0 /dev/ttyACM0 115200
+  sudo bash /home/mindmakers/programs/shells/microbit-serial-kill.sh $resposta &
+  rm -f ~/Desktop/screenlog.0
+  cd ~/Desktop
+  screen -L -Logfile screenlog.0 /dev/ttyACM0 115200
 
-sleep 5
+  sleep 5
 
 else
-echo ""
-echo ""
-echo "Microbit não conectado ao computador ou não configurado corretamente"
-sair;
+
+  echo ""
+  echo ""
+  echo "Microbit não conectado ao computador ou não configurado corretamente"
+  sair;
 
 fi
