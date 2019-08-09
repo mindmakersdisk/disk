55b4fe5b23ed4b361008cb2367a992d5b3b0f7b0
gabrielcbe <gabrielcbe@hotmail.com>
2019-08-09 09:41:55 -0300

    testes novas shells interface facilitada

--- ongoing/novas-shells/microbit-connect.sh
+++ ongoing/novas-shells/microbit-connect.sh
@@ -5,19 +5,31 @@ executa(){
 }
 
 pergunta(){
-	echo -n "Informe 'sim' para copiar o firmware que permite acessar o microbit por bluetooth. Ou enter para continuar sem copiar."
+	echo -n "Informe '1' para copiar o firmware que permite acessar o microbit por bluetooth. Ou '2' para continuar sem copiar."
 	echo ""
-	local SURE
-	read SURE
-	resposta=$SURE
+	#local SURE
+	#read SURE
+	#resposta=$SURE
+	select SURE in "Sim" "Não"; do
+	    case $yn in
+	        Sim|Não ) resposta=$SURE; break;;
+	        #Não ) echo "no"; break;;
+	    esac
+	done
 }
 
 pergunta2(){
 	echo -n "Se você viu uma 'carinha sorrindo' no mostrador, a calibragem foi efetuada com sucesso. Deseja utilizar o microbit? Recomece a rotina se houve problema."
 	echo ""
-	local SURE
-	read SURE
-	resposta=$SURE
+	#local SURE
+	#read SURE
+	#resposta=$SURE
+	select SURE in "Sim" "Não"; do
+	    case $yn in
+	        Sim|Não ) resposta=$SURE; break;;
+	        #Não ) echo "no"; break;;
+	    esac
+	done
 }
 
 sair(){
@@ -29,14 +41,14 @@ if [ -d /media/pi/MICROBIT ]
 then
 	echo "Foi detectado que o microbit está conectado via cabo. Entrando em opção de cópia de firmware..."
 	pergunta;
-	if [ "$resposta" = "sim" ] || [ "$resposta" = "s" ] || [ "$resposta" = "yes" ] || [ "$resposta" = "y" ] || [ "$resposta" = "SIM" ] || [ "$resposta" = "S" ]; then
+	if [ "$resposta" = "Sim" ]; then
 		echo "Copiando..."
 		sudo cp /home/mindmakers/programs/node_modules/bbc-microbit/firmware/node-bbc-microbit-v0.1.0.hex /media/pi/MICROBIT
 		sleep 10;
 		echo "Movimente o microbit para calibrá-lo, até desenhar um círculo em seu mostrador."
 
 		pergunta2;
-		if [ "$resposta" = "sim" ] || [ "$resposta" = "s" ] || [ "$resposta" = "yes" ] || [ "$resposta" = "y" ] || [ "$resposta" = "SIM" ] || [ "$resposta" = "S" ]; then
+		if [ "$resposta" = "Sim" ]; then
 			executa;
 		else
 			sair;
