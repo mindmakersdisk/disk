6cb410efecc6ce0a2fbae8e0e3106a02baa311a4
gabrielcbe <gabrielcbe@hotmail.com>
2019-08-30 09:35:01 -0300

    liberação imagem 3.3

--- files/programs/shells/change-shortcut2.sh
+++ files/programs/shells/change-shortcut2.sh
@@ -2,18 +2,15 @@
 
 if [ -e /home/pi/Área\ de\ Trabalho/classroom_test.desktop ]
 then
-  
+
    sudo chattr +i /home/pi/Área\ de\ Trabalho/classroom_test.desktop
-   sudo chattr +i /home/pi/Área\ de\ Trabalho/activate.desktop   
-   sudo chattr +i /home/pi/Área\ de\ Trabalho/mindmakers.desktop  
+   sudo chattr +i /home/pi/Área\ de\ Trabalho/activate.desktop
+   sudo chattr +i /home/pi/Área\ de\ Trabalho/mindmakers.desktop
 
 else
 
    sudo chattr +i /home/pi/Desktop/classroom_test.desktop
-   sudo chattr +i /home/pi/Desktop/activate.desktop   
-   sudo chattr +i /home/pi/Desktop/mindmakers.desktop   
+   sudo chattr +i /home/pi/Desktop/activate.desktop
+   sudo chattr +i /home/pi/Desktop/mindmakers.desktop
 
 fi
-
-
-
