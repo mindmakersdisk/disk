cd5aef7d3934ab2e0c16d4027886c80dfa7b45ec
Paulo <pauloalvimottoni@gmail.com>
2022-01-17 18:29:39 -0300

    shell chromium

---
+++ files/programs/shells/chromium-update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo wget  "http://archive.raspberrypi.org/debian/pool/main/c/chromium-browser/chromium-browser_88.0.4324.187-rpt1_armhf.d//eb"
+sudo wget "http://archive.raspberrypi.org/debian/pool/main/c/chromium-browser/chromium-codecs-ffmpeg-extra_88.0.4324.187-rpt1_armhf.deb"
+sudo apt install --no-install-recommends --allow-downgrades --allow-change-held-packages ./chromium-browser_88.0.4324.187-rpt1_armhf.deb ./chromium-codecs-ffmpeg-extra_88.0.4324.187-rpt1_armhf.deb -y
+
+$SHELL
