f6130ef930b8e9a8ea704e536d9d961fa6d7731b
Paulo <pauloalvimottoni@gmail.com>
2022-01-19 10:11:46 -0300

    acerto scratch3

--- files/programs/shells/chromium-fix-88.sh
+++ files/programs/shells/chromium-fix-88.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Fix para bug introduzido a partir da release 92.* do Google Chromium, que impede o Scratch3 online de funcionar por problema de Web GL
+# A Mind Makers estará acompanhando o progresso da correção para, quando oportuno, homologar release mais atual.
+# Caso essa atualização via shell esteja demorando muito: experimente baixar os dois arquivos diretamente pelo browser e, em seguida, executar o último comando a partir do diretório de download.
 
 sudo wget  "http://archive.raspberrypi.org/debian/pool/main/c/chromium-browser/chromium-browser_88.0.4324.187-rpt1_armhf.deb"
 sudo wget "http://archive.raspberrypi.org/debian/pool/main/c/chromium-browser/chromium-codecs-ffmpeg-extra_88.0.4324.187-rpt1_armhf.deb"
