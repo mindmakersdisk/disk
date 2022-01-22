e55a0046bdaa4a9bf7d68c11b4184e7637ab0007
Paulo <pauloalvimottoni@gmail.com>
2022-01-22 08:26:13 -0300

    ajustes scratch

---
+++ files/programs/shells/scratch2-remove.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Remove referencias ao Scratch2
+# Importante: não remove a pasta raiz (pendencia conhecida)
+
+sudo chattr -i -R /home/mindmakers/Scratch2
+
+sudo rm -rf /home/mindmakers/Scratch2
+
+if [ -e /home/pi/Desktop/scratch2_folder.desktop ]
+then
+sudo chattr -i -R /home/pi/Desktop/scratch2_folder.desktop
+sudo rm /home/pi/Desktop/scratch2_folder.desktop
+fi
+
+$SHELL
